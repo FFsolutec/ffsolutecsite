0caad05bbc8efaaab4df2c8c482dfc48baaa5c6e
FFsolutec <137517482+FFsolutec@users.noreply.github.com>
2024-04-24 12:25:37 -0300

    update

--- ffsolutecsite/src/pages/index.tsx
+++ ffsolutecsite/src/pages/index.tsx
@@ -1,17 +1,10 @@
 import BasicLayout from "@/@core/layout/BasicLayout";
 import HomePageView from "@/views/home";
-import InferGetStaticPropsType from "next";
 
-export default function Home({
-  cursos,
-}: InferGetStaticPropsType<typeof getStaticProps>) {
+export default function Home() {
   return (
     <BasicLayout>
       <HomePageView />
     </BasicLayout>
   );
 }
-
-export const getStaticProps = async (context: any) => {
-  return { props: { cursos: {} } };
-};
