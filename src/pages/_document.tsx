ba61b782c537f80f4ae726cf974bda5e56c2e207
FFsolutec <137517482+FFsolutec@users.noreply.github.com>
2024-07-02 16:47:15 -0300

    update

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,6 +7,20 @@ export default function Document() {
         {/* Adicione o link para o ícone aqui */}
         <link rel="icon" href="/logo.ico" />
         <title>FFsolutec</title>
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-VWW2K7GRMH"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-VWW2K7GRMH');
+            `,
+          }}
+        />
       </Head>
       <body>
         <Main />
