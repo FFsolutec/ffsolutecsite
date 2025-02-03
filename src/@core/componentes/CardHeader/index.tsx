33c49ce041781fc44b89b4f58befb461849b8a94
FFsolutec <137517482+FFsolutec@users.noreply.github.com>
2025-02-03 12:03:16 -0300

    update

--- src/@core/componentes/CardHeader/index.tsx
+++ src/@core/componentes/CardHeader/index.tsx
@@ -1,17 +1,82 @@
+import Menu from "../Menu";
+import animationData from "../../../../public/headerAnimation.json"; // Altere para o caminho correto
+import Lottie from "lottie-react";
+
 const CardHeader = () => {
   return (
-    <div className=" card-header d-flex a-s j-c f-c">
-      <h1 className="ffsolutec">FFsolutec</h1>
-      <div className="redirect-links d-flex f-wrap">
-        <p className="redirect-first">
-          Transformamos sua Visão em Realidade Digital{" "}
-        </p>
+    <div className="header">
+      <Menu />
 
-        <p className="redirect-second">
-          {" "}
-          Especialistas em Marketing Digital, Desenvolvimento de Software,
-          Criação de Sites e Suporte WordPress.
-        </p>
+      <div className="header-List">
+        <div className="card-header d-flex a-s j-c f-c">
+          <div>
+            <h1 className="welcome">Bem vindo!</h1>
+
+            <svg
+              width="400"
+              height="100"
+              viewBox="0 0 400 100"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <text
+                x="00"
+                y="60"
+                fontSize="48"
+                fontWeight="bold"
+                fill="none"
+                stroke="#21D0B3"
+                strokeWidth="2"
+                strokeDasharray="400"
+                strokeDashoffset="400"
+              >
+                FFSOLUTEC
+              </text>
+            </svg>
+
+            <style>
+              {`
+    text {
+      animation: draw 3s linear forwards;
+    }
+
+    @keyframes draw {
+      to {
+        stroke-dashoffset: 0;
+      }
+    }
+  `}
+            </style>
+
+            <div className="redirect-links d-flex f-wrap">
+              <p className="redirect-first">
+                Soluções Digitais Inteligentes para Impulsionar seu Negócio
+              </p>
+
+              <p className="redirect-second">
+                Criamos soluções digitais que geram resultados. Desenvolvimento
+                de software, websites e estratégias de marketing para
+                transformar sua empresa no digital.
+              </p>
+            </div>
+
+            <a
+              className="contact-me"
+              style={{ textDecoration: "none" }}
+              href="https://api.whatsapp.com/send?phone=5521999991286&amp;text=Bom%20dia%20gostaria%20de%20saber%20sobre%20os%20serviços%20da%20FFSOLUTEC!"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Entre em contato
+            </a>
+          </div>
+        </div>
+
+        <Lottie
+          animationData={animationData}
+          loop={true}
+          style={{ width: "100%", height: "100%" }}
+        />
       </div>
     </div>
   );
--- src/@core/layout/BasicLayout.tsx
+++ src/@core/layout/BasicLayout.tsx
@@ -2,12 +2,7 @@ import Menu from "../componentes/Menu";
 import { BlankLayoutProps } from "./types";
 
 const BasicLayout = ({ children }: BlankLayoutProps) => {
-  return (
-    <div className="layout d-flex a-c f-c j-s">
-      <Menu />
-      {children}
-    </div>
-  );
+  return <div className="layout d-flex a-c f-c j-s">{children}</div>;
 };
 
 export default BasicLayout;
